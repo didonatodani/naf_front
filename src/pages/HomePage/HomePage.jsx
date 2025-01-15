3da8715498a66a46daeba6a598d7e7fc16fe2407
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-15 14:48:10 +0100

    homepage development: services section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,8 @@
 import arrowDown from "../../assets/arrow-down.svg";
-import "./HomePage.css"
+import constructionImg from "../../assets/construction.jpg";
+import bimImg from "../../assets/bim.png";
+import woodImg from "../../assets/wood.png";
+import "./HomePage.css";
 
 import { useRef } from "react";
 
@@ -14,23 +17,64 @@ function HomePage() {
     });
   };
 
-
-
   return (
     <section className="section">
       <div className="h1-container">
         <h1>YOUR VISION, OUR MISION</h1>
-        <button onClick={()=>{scrollToSection(exploreRef)}} className="explore-btn">
-          Explore NAF <img src={arrowDown} alt="arrow down icon" id="explore-btn-arrow"/>
+        <button
+          onClick={() => {
+            scrollToSection(exploreRef);
+          }}
+          className="explore-btn"
+        >
+          Explore NAF{" "}
+          <img src={arrowDown} alt="arrow down icon" id="explore-btn-arrow" />
         </button>
       </div>
       <div className="manifesto-container" ref={exploreRef}>
         <h2>Manifesto</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi labore quam quis, illo eius excepturi? Maxime facere doloremque accusantium quidem unde necessitatibus, laboriosam officia enim neque minima, soluta illo explicabo.
-        Sed delectus accusantium temporibus aspernatur. Voluptatem, veritatis! Fugiat, perspiciatis nesciunt. Alias officiis tenetur aliquam, natus ut odit tempore? Mollitia facere autem rerum? Libero suscipit, quam dolore possimus perspiciatis quibusdam quae!
-        Totam enim consequatur dolore quasi, quae saepe deleniti praesentium est fugit commodi nulla possimus sed, a corporis. Rem aut, at, dolore quo, veniam modi possimus ex exercitationem esse necessitatibus vel.</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi labore
+          quam quis, illo eius excepturi? Maxime facere doloremque accusantium
+          quidem unde necessitatibus, laboriosam officia enim neque minima,
+          soluta illo explicabo. Sed delectus accusantium temporibus aspernatur.
+          Voluptatem, veritatis! Fugiat, perspiciatis nesciunt. Alias officiis
+          tenetur aliquam, natus ut odit tempore? Mollitia facere autem rerum?
+          Libero suscipit, quam dolore possimus perspiciatis quibusdam quae!
+          Totam enim consequatur dolore quasi, quae saepe deleniti praesentium
+          est fugit commodi nulla possimus sed, a corporis. Rem aut, at, dolore
+          quo, veniam modi possimus ex exercitationem esse necessitatibus vel.
+        </p>
       </div>
-      <div></div>
+      <div className="services-container">
+        <h2>Our Services</h2>
+        <div className="services-article-container">
+          <article className="services-article">
+            <h3 className="services-article-title">Construction →</h3>
+            <img
+              src={constructionImg}
+              alt="construction image"
+              className="services-article-img"
+            />
+          </article>
+          <article className="services-article">
+            <h3 className="services-article-title">Wood Frame →</h3>
+            <img
+              src={woodImg}
+              alt="wood frame image"
+              className="services-article-img"
+            />
+          </article>
+          <article className="services-article">
+            <h3 className="services-article-title">BIM Design →</h3>
+            <img
+              src={bimImg}
+              alt="BIM image"
+              className="services-article-img"
+            />
+          </article>
+        </div>
+      </div>
     </section>
   );
 }
