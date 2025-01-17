f1d17bb11c84383814b19f9ff6faf639926ab364
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-01-17 17:23:05 +0100

    some structure and styles added to about page and staf cards

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -1,9 +1,17 @@
-function AboutCard(){
-    return(
-        <div>
-            about card
-        </div>
-    )
+import "./AboutCard.css"
+import linkedinIcon from "../../assets/linkedin.png"
+
+function AboutCard({ member }) {
+    const {fullName, position, profilePic, linkedinUrl } = member;
+
+  return (
+    <article className="staff-member-card">
+      <img src={profilePic} alt="staff member profile pic" className="staff-pic"/>
+      <h2>{fullName}</h2>
+      <p>{position}</p>
+      <a href={linkedinUrl} target="_blank" className="linkedin-link"><img src={linkedinIcon} alt="linkedin button" className="linkedin-link"/></a>
+    </article>
+  );
 }
 
-export default AboutCard
+export default AboutCard;
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,37 @@
-function AboutPage(){
-    return (
-      <section className="section">AboutPage</section>
-    )
-  }
-  
-  export default AboutPage
+import AboutCard from "../../components/AboutCard/AboutCard";
+import partners from "../../data/partners.json";
+import staff from "../../data/staff.json";
+
+import "./AboutPage.css"
+
+function AboutPage() {
+  return (
+    <section className="about-section section">
+      <h1>WE ARE....</h1>
+      <p>
+        manifesto Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatem laboriosam, id quasi laborum quibusdam possimus sunt
+        reiciendis, ab est sed maxime nihil quisquam quas ipsum error temporibus
+        dolore, libero accusantium! Modi mollitia ab dolorem illo quo
+        blanditiis, magni aliquam vitae obcaecati enim culpa, quos magnam quam
+        vero eveniet exercitationem saepe qui repellat! Consectetur nisi, facere
+        rerum impedit quo dolorem minima!
+      </p>
+      <h2>OUR STAFF</h2>
+      <div className="staff-container">
+        {partners.map((member, index) => {
+          return <AboutCard key={index} member={member} />;
+        })}
+      </div>
+      <div className="staff-container">
+        {
+          staff.map((member, index)=>{
+            return <AboutCard key={index} member={member}/>
+          })
+        }
+      </div>
+    </section>
+  );
+}
+
+export default AboutPage;
